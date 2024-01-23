7fc142497e63c5d3d9e8ea92ff448bb5cca11c1a
DiegoR30 <die-35_45rhotmail.com>
2024-01-23 01:02:49 -0500

    ingreso de styles

--- hoteles/includes/login.php
+++ hoteles/includes/login.php
@@ -7,24 +7,26 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>login</title>
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="../css/styles.css">
+
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    
 	
 
 </head>
 
-<body>
-<form  action="_functions.php" method="POST">
-<div id="login" >
-        <div class="container">
-            <div id="login-row" class="row justify-content-center align-items-center">
-                <div id="login-column" class="col-md-6">
-                    <div id="login-box" class="col-md-12">
-                        <br>
-           
-                   <br>
-                                    <h3 class="text-center">Iniciar Sesión</h3>
-                       <br>
+<body class= body>
+    <form  action="_functions.php" method="POST">
+        <div id="login" >
+            <div class="container">
+                <div id="login-row" class="row justify-content-center align-items-center">
+                    <div id="login-column" class="col-md-6">
+                        <div id="login-box" class="col-md-12">
+                            <br>
+                            <br>
+                            <h3 class="text-center">Iniciar Sesión</h3>
+                            <br>
                             <div class="form-group">
                                 <label for="correo">Usuario:</label><br>
                                 <input type="text" name="nombre" id="nombre" class="form-control" required>
@@ -35,16 +37,16 @@
                                 <input type="hidden" name="accion" value="acceso_user">
                             </div>
                             <div class="form-group">
-                             <br>
-                    <center>
+                                <br>
+
                                 <input type="submit"class="btn btn-success" value="Ingresar">   
-                                </center>
-                        </form>
+
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </div>
+        </div>    
     </form>
 </body>
 </html>
